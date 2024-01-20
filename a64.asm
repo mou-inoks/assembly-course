ea4c3dae4065c5ecb9487a789d5ddf6425d8a73b
mou-inoks <mou@inokscapital.com>
2024-01-20 20:12:57 +0100

    fuck assembly

--- a64.asm
+++ a64.asm
@@ -2,11 +2,16 @@ bits 64
 
 section .data 
     message db 'Hello world !', 10 
+
 section .text 
-    global _start 
-    _start 
+    global _start
+    _start
         mov rax, 1
         mov rdi, 1
         mov rsi, message ; message 
-        mov rdx, 13+1  ;nombres de caractères du message 
+        mov rdx, 13 ;nombres de caractères du message
         syscall ;Anaylser tout le code du dessus pour l'exec
+
+        mov rax, 60 ; process pour quitter le programme
+        mov rdi, 0
+        syscall 
